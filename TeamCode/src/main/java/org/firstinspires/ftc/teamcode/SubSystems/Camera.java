360e8548c9165cd57796e3e676d6cccbcaba3685
Ikuma Mitsubori <ikuma0245@gmail.com>
2024-01-11 08:59:47 -0700

    add Camera.javan test (NOT CHECKED)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/Camera.java
@@ -200,6 +200,7 @@ public class Camera extends LinearOpMode
                             input.rows()*(3f/4f)),
                     new Scalar(0, 255, 0), 4);
 
+
             /**
              * NOTE: to see how to get data from your pipeline to your OpMode as well as how
              * to change which stage of the pipeline is rendered to the viewport when it is
